0a47cb8bb3ef9271f859974b3c851f53a78be904
Jomiloju David <34211095+JomilojuDavid@users.noreply.github.com>
2025-10-25 15:06:45 +0100

    Refactor input styles for Auth component
    Updated input styles to use normal font weight and adjusted placeholder text sizes.

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -199,10 +199,10 @@ export default function Auth() {
               placeholder="Your Email"
               value={signInEmail}
               onChange={(e) => setSignInEmail(e.target.value)}
-              className="h-[65px] sm:h-[70px] rounded-[0px] border border-[#DF1516] 
-                         text-[30px] font-semibold font-['Poppins'] 
-                         placeholder:text-[32px] placeholder:font-semibold 
-                         focus-visible:ring-[#DF1516]"
+              className="h-[65px] sm:h-[70px] rounded-[0px] border border-[#DF1516]
+                         text-[26px] font-normal font-['Poppins'] 
+                         placeholder:text-[26px] placeholder:font-normal
+                         placeholder:text-[#DF1516] focus-visible:ring-[#DF1516]"
             />
             <div className="relative">
               <Input
@@ -211,8 +211,9 @@ export default function Auth() {
                 value={signInPassword}
                 onChange={(e) => setSignInPassword(e.target.value)}
                 className="h-[65px] sm:h-[70px] rounded-[0px] border border-[#DF1516] 
-                           pr-[150px] text-[30px] font-semibold font-['Poppins'] 
-                           placeholder:text-[32px] placeholder:font-semibold"
+                           pr-[150px] text-[26px] font-normal font-['Poppins']
+                           placeholder:text-[26px] placeholder:font-normal
+                           placeholder:text-[#DF1516]"
               />
               <Button
                 type="submit"
@@ -281,8 +282,8 @@ export default function Auth() {
                 onChange={(e) => setFullName(e.target.value)}
                 className="h-[75px] sm:h-[83px] rounded-[0px] border border-white 
                            bg-white/95 text-black placeholder:text-gray-500 
-                           text-[26px] font-semibold font-['Poppins'] 
-                           placeholder:text-[22px] placeholder:font-semibold"
+                           text-[26px] font-normal font-['Poppins'] 
+                           placeholder:text-[26px] placeholder:font-normal"
               />
               <Input
                 type="email"
@@ -291,8 +292,8 @@ export default function Auth() {
                 onChange={(e) => setSignUpEmail(e.target.value)}
                 className="h-[75px] sm:h-[83px] rounded-[0px] border border-white 
                            bg-white/95 text-black placeholder:text-gray-500 
-                           text-[26px] font-semibold font-['Poppins'] 
-                           placeholder:text-[22px] placeholder:font-semibold"
+                           text-[26px] font-normal font-['Poppins'] 
+                           placeholder:text-[26px] placeholder:font-normal"
               />
             </div>
 
@@ -303,8 +304,8 @@ export default function Auth() {
               onChange={(e) => setSignUpPassword(e.target.value)}
               className="h-[75px] sm:h-[83px] w-full rounded-[0px] border border-white 
                          bg-white/95 text-black placeholder:text-gray-500 
-                         text-[26px] font-semibold font-['Poppins'] 
-                         placeholder:text-[22px] placeholder:font-semibold"
+                         text-[26px] font-normal font-['Poppins'] 
+                         placeholder:text-[26px] placeholder:font-normal"
             />
 
             <Button
