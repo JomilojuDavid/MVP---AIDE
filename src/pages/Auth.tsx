2164689ebea455de65f3b5abc800ec1b57587a18
Jomiloju Wale-Adegboye <34211095+JomilojuDavid@users.noreply.github.com>
2025-12-05 19:44:16 +0100

    Update Auth.tsx

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -110,7 +110,7 @@ export default function Auth() {
     try {
       const { error } = await supabase.auth.signInWithOAuth({
         provider: "google",
-        options: { redirectTo: `${window.location.origin}/quiz-step2` },
+        options: { redirectTo: `${window.location.origin}/dashboard` },
       });
       if (error) throw error;
     } catch (error: any) {
