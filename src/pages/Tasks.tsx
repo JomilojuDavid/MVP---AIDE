9fabd962998c4be1d89dea498fb0c64555b19d35
Jomiloju Wale-Adegboye <34211095+JomilojuDavid@users.noreply.github.com>
2026-01-08 19:47:49 +0100

    Tasks retouch

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -89,7 +89,7 @@ export default function Tasks() {
               padding: "24px 36px",
             }}
           >
-            <p style={{ fontFamily: "Arial", fontSize: 45 }}>
+            <p style={{ fontFamily: "Arial", fontSize: 45, lineHeight: "100%" }}>
               Complete Your AIDE Tasks,{" "}
               <span style={{ color: "#DF1516" }}>{firstName || "Name"}!</span>
             </p>
@@ -108,61 +108,171 @@ export default function Tasks() {
               height: 439,
               background: "#fff",
               boxShadow: "0px 4px 4px #00000040",
-              position: "relative", // ✅ FIX
+              position: "relative",
             }}
           >
-            {[
-              { y: 53, title: "Complete Awareness Journal", body: "Reflect on your current mindset and note key insights." },
-              { y: 143, title: "Set weekly intention", body: "Define 3 clear goals for the week to stay aligned." },
-              { y: 239, title: "Make One Decisive Move", body: "Take one action that moves you closer to your goal." },
-              { y: 339, title: "Execute Your Action Plan", body: "Put your weekly plan into motion and review results." },
-            ].map((item, i) => (
-              <div key={i}>
-                <input
-                  type="checkbox"
-                  style={{
-                    position: "absolute",
-                    top: item.y,
-                    left: 67,
-                    width: 28,
-                    height: 24,
-                    border: "1px solid #000",
-                    appearance: "none",
-                  }}
-                />
+            {/* TASK 1 */}
+            <input
+              type="checkbox"
+              style={{
+                position: "absolute",
+                top: 53,   // 338 - 285
+                left: 67,  // 439 - 372
+                width: 28,
+                height: 24,
+                border: "1px solid #000",
+                appearance: "none",
+              }}
+            />
 
-                <p
-                  style={{
-                    position: "absolute",
-                    top: item.y - 6,
-                    left: 117,
-                    fontFamily: "Montserrat",
-                    fontSize: 24,
-                    fontWeight: 500,
-                  }}
-                >
-                  {item.title}
-                </p>
+            <p
+              style={{
+                position: "absolute",
+                top: 47,   // 332 - 285
+                left: 117, // 489 - 372
+                fontFamily: "Montserrat",
+                fontSize: 24,
+                fontWeight: 500,
+              }}
+            >
+              Complete Awareness Journal
+            </p>
 
-                <p
-                  style={{
-                    position: "absolute",
-                    top: item.y + 45,
-                    left: 119,
-                    fontFamily: "Montserrat",
-                    fontSize: 18,
-                  }}
-                >
-                  {item.body}
-                </p>
-              </div>
-            ))}
+            <p
+              style={{
+                position: "absolute",
+                top: 98,   // 383 - 285
+                left: 119, // 491 - 372
+                fontFamily: "Montserrat",
+                fontSize: 18,
+              }}
+            >
+              Reflect on your current mindset and note key insights.
+            </p>
 
+            {/* TASK 2 */}
+            <input
+              type="checkbox"
+              style={{
+                position: "absolute",
+                top: 143, // 428 - 285
+                left: 65,
+                width: 28,
+                height: 24,
+                border: "1px solid #000",
+                appearance: "none",
+              }}
+            />
+
+            <p
+              style={{
+                position: "absolute",
+                top: 137,
+                left: 115,
+                fontFamily: "Montserrat",
+                fontSize: 24,
+                fontWeight: 500,
+              }}
+            >
+              Set Weekly Intention
+            </p>
+
+            <p
+              style={{
+                position: "absolute",
+                top: 188,
+                left: 117,
+                fontFamily: "Montserrat",
+                fontSize: 18,
+              }}
+            >
+              Define 3 clear goals for the week to stay aligned.
+            </p>
+
+            {/* TASK 3 */}
+            <input
+              type="checkbox"
+              style={{
+                position: "absolute",
+                top: 239,
+                left: 65,
+                width: 28,
+                height: 24,
+                border: "1px solid #000",
+                appearance: "none",
+              }}
+            />
+
+            <p
+              style={{
+                position: "absolute",
+                top: 233,
+                left: 115,
+                fontFamily: "Montserrat",
+                fontSize: 24,
+                fontWeight: 500,
+              }}
+            >
+              Make One Decisive Move
+            </p>
+
+            <p
+              style={{
+                position: "absolute",
+                top: 284,
+                left: 117,
+                fontFamily: "Montserrat",
+                fontSize: 18,
+              }}
+            >
+              Take one action that moves you closer to your goal.
+            </p>
+
+            {/* TASK 4 */}
+            <input
+              type="checkbox"
+              style={{
+                position: "absolute",
+                top: 339,
+                left: 65,
+                width: 28,
+                height: 24,
+                border: "1px solid #000",
+                appearance: "none",
+              }}
+            />
+
+            <p
+              style={{
+                position: "absolute",
+                top: 333,
+                left: 115,
+                fontFamily: "Montserrat",
+                fontSize: 24,
+                fontWeight: 500,
+              }}
+            >
+              Execute Your Action Plan
+            </p>
+
+            <p
+              style={{
+                position: "absolute",
+                top: 384,
+                left: 117,
+                fontFamily: "Montserrat",
+                fontSize: 18,
+              }}
+            >
+              Put your weekly plan into motion and review results.
+            </p>
+
+            {/* START TASKS BUTTON */}
             <Button
               style={{
                 position: "absolute",
-                bottom: 24,
-                right: 24,
+                top: 376,  // 661 - 285
+                left: 741, // 1113 - 372
                 width: 203,
                 height: 44,
                 borderRadius: 17,
